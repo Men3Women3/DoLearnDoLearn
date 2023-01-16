f84806aed121661957c028478d3f60fb919f0dfa
찬휘 <cksgnlcjswoo@naver.com>
2023-01-16 12:32:38 +0900

    file setup

---
+++ back-end/src/main/java/com/example/dolearn/DolearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.dolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolearnApplication.class, args);
+	}
+
+}
