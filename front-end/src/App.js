c2e5bc3998814786a7d1207cc1a17ac81083f0ad
unknown <pjw2369@naver.com>
2023-01-16 14:11:11 +0900

    setting

---
+++ front-end/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import pMinDelay from "p-min-delay";
+import { Suspense } from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import Loading from "./pages/Loading";
+
+// 코드 스플리팅 (Code Splitting)
+const Home = React.lazy(() => pMinDelay(import("./pages/Home/index"), 1000));
+const Login = React.lazy(() => pMinDelay(import("./pages/Login/index"), 1000));
+const SingUp = React.lazy(() =>
+  pMinDelay(import("./pages/SignUp/index"), 1000)
+);
+const NotFound = React.lazy(() =>
+  pMinDelay(import("./pages/NotFound/index"), 1000)
+);
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Suspense fallback={<Loading />}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SingUp />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ThemeProvider>
+  );
+}
+
+export default App;
