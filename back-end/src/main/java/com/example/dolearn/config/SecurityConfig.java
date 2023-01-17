451f610b4be9ae237f2ff917e24722fbf7333b64
angly97 <angly97@naver.com>
2023-01-17 10:39:19 +0900

    feat : spring security 기본 설정

---
+++ back-end/src/main/java/com/example/dolearn/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.dolearn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // jwt 사용을 위한 기본 설정
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+
+                .authorizeRequests()
+                .anyRequest().permitAll()
+
+                // cors 설정 적용
+                .and()
+                .cors().configurationSource(corsConfigurationSource());
+        return http.build();
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);       // 서버의 json 응답을 JS로 처리가능하게 함
+        config.addAllowedOriginPattern("*");    // springboot cors 설정 시, allowCredentials(true)와 allowedOrigin("*") 같이 사용 불가하게 업뎃
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoderPassword() {
+        return new BCryptPasswordEncoder();
+    }
+}
