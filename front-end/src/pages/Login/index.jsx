fb5d7f5b8a04a25fb36db938109b8f5016f384a8
unknown <pjw2369@naver.com>
2023-01-17 14:08:08 +0900

    update : 로그인 페이지 업데이트

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
 import loginImg from "../../assets/images/login_img.svg";
 import logoImg from "../../assets/images/logo.png";
-import { SMain, SForm, SImgSection, SContainer, SSNSContainer, SInputContainer, SEmailFontAwesomeIcon, SEmailInput, SPasswordInput, SLoginButton, SNaverContainer, SKakaoContainer, SGoogleContainer, SMainContainer, SSignUpButton, SFindPassword } from "./styles";
-import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
-import { NavLink } from 'react-router-dom';
-import TransitionsModal from '../../components/Modal';
-
+import {
+  SMain,
+  SForm,
+  SImgSection,
+  SContainer,
+  SSNSContainer,
+  SInputContainer,
+  SEmailFontAwesomeIcon,
+  SEmailInput,
+  SPasswordInput,
+  SLoginButton,
+  SNaverContainer,
+  SKakaoContainer,
+  SGoogleContainer,
+  SMainContainer,
+  SSignUpButton,
+  SFindPassword,
+} from "./styles";
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import TransitionsModal from "../../components/Modal";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isEmpty, setIsEmpty] = useState('');
-  const [isCorrect, setIsCorrect] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isEmpty, setIsEmpty] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
 
-  
   return (
     <SMain>
       {/* <TransitionsModal /> */}
       <SMainContainer>
-        <NavLink to={'/'} >
+        <NavLink to={"/"}>
           <img src={logoImg} alt="logo_img" />
         </NavLink>
         <SImgSection>
@@ -29,17 +44,35 @@ const Login = () => {
           <SContainer>
             <h1>로그인</h1>
             <SInputContainer>
-              <SEmailInput className={email ? 'active__input' : ''} value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="이메일을 입력해주세요"/>
-              <SEmailFontAwesomeIcon className={email ? 'active__icon' : ''} icon={faEnvelope} />
+              <SEmailInput
+                className={email ? "active__input" : ""}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+                placeholder="이메일을 입력해주세요"
+              />
+              <SEmailFontAwesomeIcon
+                className={email ? "active__icon" : ""}
+                icon={faEnvelope}
+              />
             </SInputContainer>
             <SInputContainer>
-              <SPasswordInput className={password ? 'active__input' : ''} value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="비밀번호를 입력해주세요" />
-              <SEmailFontAwesomeIcon className={password ? 'active__icon' : ''} icon={faLock} />
+              <SPasswordInput
+                className={password ? "active__input" : ""}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+              />
+              <SEmailFontAwesomeIcon
+                className={password ? "active__icon" : ""}
+                icon={faLock}
+              />
             </SInputContainer>
             <SFindPassword>
               <p>비밀번호 찾기</p>
             </SFindPassword>
-            <SLoginButton type='submit'>로그인</SLoginButton>
+            <SLoginButton type="submit">로그인</SLoginButton>
             <SSignUpButton>회원가입</SSignUpButton>
             <SSNSContainer>
               <SNaverContainer>
@@ -54,7 +87,6 @@ const Login = () => {
             </SSNSContainer>
           </SContainer>
         </SForm>
-
       </SMainContainer>
     </SMain>
   );
