73209780985a1528d94eebe386cd425ad20c5ab7
unknown <wjddnjs0528@gmail.com>
2023-01-18 13:16:53 +0900

    update : 프로필 사이드바 반응형으로 변경

--- front-end/src/pages/User/index.jsx
+++ front-end/src/pages/User/index.jsx
@@ -5,17 +5,25 @@ import CardBox from "../../components/CardBox";
 import Profile from "../../components/Profile/index";
 import ProfileSidebar from "../../components/ProfileSidebar";
 
+import Grid from "@mui/material/Grid";
+
 const User = () => {
   return (
     <div>
       <Navbar />
-      <div style={{ display: "flex", width: "100vw" }}>
-        <ProfileSidebar />
+      {/* <div style={{ display: "flex", width: "100vw" }}> */}
+      <Grid container>
+        <Grid item xs={2.5}>
+          <ProfileSidebar />
+        </Grid>
+        <Grid item xs={9.5} style={{ alignItems: "center" }}>
+          <CardBox>
+            <Profile />
+          </CardBox>
+        </Grid>
+      </Grid>
 
-        <CardBox>
-          <Profile />
-        </CardBox>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
