84e294a040ae31c56098b6d9e486c9aef596cca6
young <moxnox63@naver.com>
2023-01-18 17:50:26 +0900

    update: 강의 요청 페이지 정렬

--- front-end/src/pages/WriteBoard/styles.jsx
+++ front-end/src/pages/WriteBoard/styles.jsx
@@ -6,21 +6,57 @@ export const SContainer = styled.div`
   margin: 10%;
 `;
 
-// 페이지 제목
+// 1. 페이지 제목
 export const STitle = styled.div`
   font-size: calc(1.5vw + 1px);
   border-bottom: 1px solid black;
   margin-bottom: 1%;
 `;
 
-// 사용자 지정 제목 - custom-title
+// 3. 사용자 지정 제목 - custom-title
 export const SBoardTitle = styled.div`
   font-size: calc(1.5vw + 1px);
+  display: flex;
+  h3 {
+    /* display: inline-block;   */
+    margin-right: 15px;
+  }
 `;
 
-// 참여 인원 - people-number
+// 4. 참여 인원 - people-number
 export const SParticipant = styled.div`
   font-size: calc(1.5vw + 1px);
+  display: flex;
+  h3 {
+    margin-right: 15px;
+  }
+  h5 {
+    margin-left: 10px;
+  }
+`;
+
+// 5. 모집 기간 - recruit-period
+export const SRecruit = styled.div`
+  font-size: calc(1.5vw + 1px);
+  display: flex;
+  h3 {
+    margin-right: 15px;
+  }
+`;
+
+// 6. 강의 일시
+export const SLecture = styled.div`
+  font-size: calc(1.5vw + 1px);
+  display: flex;
+  h3 {
+    margin-right: 15px;
+  }
+`;
+
+// 글자수 제한 p태그
+export const SLimit = styled.p`
+  text-align: right;
+  font-size: calc(1vw + 1px);
 `;
 
 // 요청내용 요약 - request-summary
