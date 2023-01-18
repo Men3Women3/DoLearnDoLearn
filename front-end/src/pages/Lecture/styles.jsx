a5c19ee96475fbeebfa48c5f0235951d49ef76b7
unknown <pjw2369@naver.com>
2023-01-18 17:30:24 +0900

    feat : 라이브 강의 페이지 생성

--- front-end/src/pages/Lecture/styles.jsx
+++ front-end/src/pages/Lecture/styles.jsx
@@ -1,1 +1,8 @@
-// import styled from "styled-components";
+import styled from "styled-components";
+
+export const SMainContainer = styled.main`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+`;
