eb28c5241c2fd3044c1970183b317fecd2642d9a
unknown <pjw2369@naver.com>
2023-01-19 11:25:56 +0900

    update : 폰트 추가

--- front-end/src/theme.js
+++ front-end/src/theme.js
@@ -4,9 +4,18 @@ const theme = {
   lightYellow: "#FFF9DB",
   lighterYellow: "#FFFAEC",
   red: "#FF7979",
-  h1Size: "",
-  fontFamily: "KIMM_Bold",
-  // fontSize: "calc(2vw + 5px)",
+  // h1Size: "calc()",
+  fontFamily: {
+    Thin: "Pretendard-Thin",
+    ExtraLight: "Pretendard-ExtraLight",
+    Light: "Pretendard-Light",
+    Regular: "Pretendard-Regular",
+    Medium: "Pretendard-Medium",
+    SemiBold: "Pretendard-SemiBold",
+    Bold: "Pretendard-Bold",
+    ExtraBold: "Pretendard-ExtraBold",
+    Black: "Pretendard-Black",
+  },
 };
 
 export default theme;
