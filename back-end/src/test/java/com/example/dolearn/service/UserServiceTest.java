614c08de561bcbf8e17c677f8f7b093735378b82
angly97 <angly97@naver.com>
2023-01-22 00:15:09 +0900

    feat : 로그인 테스트 구현

--- back-end/src/test/java/com/example/dolearn/service/UserServiceTest.java
+++ back-end/src/test/java/com/example/dolearn/service/UserServiceTest.java
@@ -74,4 +74,72 @@ class UserServiceTest {
             assertEquals(exception.getMessage(), "이미 존재하는 이메일입니다.");
         }
     }
+
+    @Nested
+    class login{
+
+        private String name;
+        private String email;
+        private String password;
+
+        @BeforeEach
+        void setup(){
+            // DB에 없는 데이터로 초기화
+            name = "가입자명";
+            email = "abcd@daum.net";
+            password = "abcdpassord";
+        }
+
+        @Test
+        @DisplayName("로그인 성공")
+        void success() {
+            name = "민싸피";
+            email = "ssafy@naver.com";
+            password = "$2a$10$Ul5NqDvSKVE/pbZhr5TlduldDXNk8ZztWAg8gWGxjB0zuK9tn9QHy";
+
+            UserDto reqUserDto = UserDto.builder().email(email).password("abcd!1234").build();
+            UserDto loginuUserDto = UserDto.builder().name(name).email(email).password(password).build();
+
+            when(userRepository.findOneByEmail(email)).thenReturn(Optional.ofNullable(loginuUserDto.toEntity()));
+            when(passwordEncoder.matches("abcd!1234", password)).thenReturn(true);
+
+            UserDto userDto = userService.login(reqUserDto);
+
+            assertEquals(name, userDto.getName());
+        }
+
+        @Test
+        @DisplayName("로그인 실패 - 중복된 이메일")
+        void failByEmail() {
+            UserDto userDto = UserDto.builder().email(email).password(password).build();
+
+            when(userRepository.findOneByEmail(email)).thenReturn(Optional.ofNullable(userDto.toEntity()));
+
+            Exception exception = assertThrows(CustomException.class, ()->{
+                UserDto result = userService.login(userDto);
+            });
+
+            assertTrue(exception instanceof CustomException);
+            assertEquals(exception.getMessage(), "없는 사용자입니다.");
+        }
+
+        @Test
+        @DisplayName("로그인 실패 - 잘못된 비밀번호")
+        void failByPassword() {
+            password = "$2a$10$Ul5NqDvSKVE/pbZhr5TlduldDXNk8ZztWAg8gWGxjB0zuK9tn9QHy";
+
+            UserDto reqUserDto = UserDto.builder().email(email).password("이상한 비번").build();
+            UserDto loginuUserDto = UserDto.builder().name(name).email(email).password(password).build();
+
+            when(userRepository.findOneByEmail(email)).thenReturn(Optional.ofNullable(loginuUserDto.toEntity()));
+            when(passwordEncoder.matches("이상한 비번", password)).thenReturn(false);
+
+            Exception exception = assertThrows(CustomException.class, ()->{
+                UserDto result = userService.login(reqUserDto);
+            });
+
+            assertTrue(exception instanceof CustomException);
+            assertEquals(exception.getMessage(), "없는 사용자입니다.");
+        }
+    }
 }
