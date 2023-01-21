2d29bede29eeffdb6b440c4a3d0863166a2a55f9
angly97 <angly97@naver.com>
2023-01-22 00:14:50 +0900

    feat : 회원가입 실패 테스트 구현

--- back-end/src/test/java/com/example/dolearn/service/UserServiceTest.java
+++ back-end/src/test/java/com/example/dolearn/service/UserServiceTest.java
@@ -2,14 +2,19 @@ package com.example.dolearn.service;
 
 import com.example.dolearn.domain.User;
 import com.example.dolearn.dto.UserDto;
+import com.example.dolearn.exception.CustomException;
 import com.example.dolearn.repository.UserRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -22,6 +27,9 @@ class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     @Nested
     class signup{
 
@@ -31,20 +39,39 @@ class UserServiceTest {
 
         @BeforeEach
         void setup(){
+            // DB에 없는 데이터로 초기화
             name = "가입자명";
             email = "abcd@daum.net";
             password = "abcdpassord";
         }
 
         @Test
-        @DisplayName("새로운 사용자 회원가입")
+        @DisplayName("회원가입 성공")
         void success() {
             UserDto userDto = UserDto.builder().name(name).email(email).password(password).build();
+
             when(userRepository.save(any(User.class))).thenReturn(userDto.toEntity());
 
             UserDto result = userService.signup(userDto);
 
             assertThat(result.getEmail()).isEqualTo(email);
+        }
+
+        @Test
+        @DisplayName("회원가입 실패 - 중복된 이메일")
+        void failByEmail() {
+            email = "ssafy@naver.com";
+
+            UserDto userDto = UserDto.builder().name(name).email(email).password(password).build();
+
+            when(userRepository.findOneByEmail(email)).thenReturn(Optional.ofNullable(userDto.toEntity()));
+
+            Exception exception = assertThrows(CustomException.class, ()->{
+                UserDto result = userService.signup(userDto);
+            });
+
+            assertTrue(exception instanceof CustomException);
+            assertEquals(exception.getMessage(), "이미 존재하는 이메일입니다.");
         }
     }
 }
