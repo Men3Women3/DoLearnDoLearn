5fe64ca27ca558d5b229677d9b0a304dc86d67b5
unknown <pjw2369@naver.com>
2023-01-24 00:43:31 +0900

    update : UI 수정

--- front-end/src/components/LectureCameraContainer/styles.jsx
+++ front-end/src/components/LectureCameraContainer/styles.jsx
@@ -1,24 +1,21 @@
 import styled from "styled-components";
 
 export const SContainer = styled.section`
-  width: 75%;
+  width: 100%;
+  margin: calc(1vw + 8px);
 `;
 
 export const SStudentsContainer = styled.div`
   display: flex;
-  text-align: center;
+  justify-content: center;
   height: 20%;
+  text-align: center; // 나중에 지워야 될 스타일
   div {
     background-color: white;
     width: 20%;
-    margin: 16px 6px;
-    border-radius: 4px;
-  }
-  .first-user {
-    margin-left: 16px;
-  }
-  .last-user {
-    margin-right: 16px;
+    margin: calc(1vh + 12px) calc(0.5vw + 2px);
+    margin-top: 0px;
+    border-radius: 12px;
   }
 `;
 
@@ -33,49 +30,65 @@ export const SLecturerCamera = styled.div`
   width: 100%;
   height: 100%;
   background-color: white;
-  border-radius: 4px;
-  margin: 0px 12px;
+  border-radius: 12px;
+  margin: 0px calc(0.5vw + 2px);
 `;
 
 export const SOptionContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  select {
-    border: none;
-    background-color: transparent;
-    outline: none;
-  }
-  .icon {
-    width: calc(1vw + 10px);
-    height: calc(1vh + 10px);
-  }
-  .big-icon {
-    width: calc(1vw + 10px);
-    height: calc(1vh + 10px);
-  }
-  .camera-icon {
-    transform: translateX(32px);
-  }
-  .mike-icon {
-    transform: translateX(32px);
-  }
-  .exit-button:hover {
-    background-color: #e53e3e;
-    .exit-icon {
-      color: white;
-    }
-  }
+  background-color: #242424;
+  text-align: center;
+  margin: auto;
+  width: 65%;
+  margin-top: calc(0.8vw + 2px);
+  border-radius: 8px;
   button {
     cursor: pointer;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    margin: 10px;
-    background-color: #d9d9d9;
+    margin: calc(0.5vw + 2px);
+    background-color: transparent;
     width: 10%;
     height: 10%;
-    border-radius: 20px;
-    padding: 10px;
+    border-radius: 8px;
+    padding: calc(0.8vw + 2px) calc(1vh + 12px);
+    border: none;
+    outline: none;
+  }
+  button:hover {
+    background-color: #585858;
+    .icon {
+      color: ${(props) => props.theme.deeperYellow};
+    }
+  }
+  /* select {
+    border: none;
+    background-color: transparent;
+    outline: none;
+  } */
+  .icon {
+    width: calc(1vw + 12px);
+    height: calc(1vh + 12px);
+    color: #7e7e7e;
+  }
+  /* .big-icon {
+    width: calc(1vw + 12px);
+    height: calc(1vh + 12px);
+  } */
+  /* .camera-icon {
+    transform: translateX(32px);
+  }
+  .mike-icon {
+    transform: translateX(32px);
+  } */
+  .exit-button:hover {
+    background-color: #e53e3e;
+    opacity: 1;
+    .exit-icon {
+      color: white;
+    }
   }
 `;
--- front-end/src/components/LectureChattingContainer/index.jsx
+++ front-end/src/components/LectureChattingContainer/index.jsx
@@ -14,6 +14,21 @@ import thumbnailImg from "../../assets/images/thumbnail.png";
 const LectureChattingContainer = () => {
   return (
     <SContainer>
+      <SChattingContainer>
+        <SContentCantainer>
+          <SChattingContent>
+            <p>김싸피 (수강생)</p>
+            <div>다시 설명해주세요!</div>
+          </SChattingContent>
+        </SContentCantainer>
+        <SMessageContainer>
+          <textarea cols="30" rows="3" />
+          {/* 메시지 전송 버튼 */}
+          <button>
+            <FontAwesomeIcon className="send-icon" icon={faPaperPlane} />
+          </button>
+        </SMessageContainer>
+      </SChattingContainer>
       <SUsersContainer>
         <span>
           <img className="lecturer" src={thumbnailImg} alt="" />
@@ -34,20 +49,6 @@ const LectureChattingContainer = () => {
           <img src={thumbnailImg} alt="" />
         </span>
       </SUsersContainer>
-      <SChattingContainer>
-        <SContentCantainer>
-          <SChattingContent>
-            <p>김싸피 (수강생)</p>
-            <div>다시 설명해주세요!</div>
-          </SChattingContent>
-        </SContentCantainer>
-        <SMessageContainer>
-          <textarea cols="30" rows="5" />
-          <button>
-            <FontAwesomeIcon className="send-icon" icon={faPaperPlane} />
-          </button>
-        </SMessageContainer>
-      </SChattingContainer>
     </SContainer>
   );
 };
