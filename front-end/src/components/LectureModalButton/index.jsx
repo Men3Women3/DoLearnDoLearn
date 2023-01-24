99f4866ab33db4f57b9d0ed0a6187c4d00cd732f
moxnox63 <moxnox63@gmail.com>
2023-01-24 11:16:00 +0900

    update: 강의 상세 모달 수정

--- front-end/src/components/LectureModalButton/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { SLectureModalButton } from "./styles";
-
-const LectureModalButton = () => {
-  return <SLectureModalButton></SLectureModalButton>;
-};
-
-export default LectureModalButton;
