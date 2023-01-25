0cd6669af1a97235fc7534d4ae179f779642153b
unknown <pjw2369@naver.com>
2023-01-25 11:03:57 +0900

    update : merge

--- front-end/src/components/Typing/index.jsx
+++ front-end/src/components/Typing/index.jsx
@@ -8,7 +8,6 @@ const Typing = () => {
   const [typingChangeFlag, setTypingChangeFlag] = useState(false);
   const typingContent = "배우고, 나누고, 성장해요";
 
-  // 후보1
   useEffect(() => {
     let typingInterval;
     if (!typingChangeFlag) {
@@ -25,98 +24,39 @@ const Typing = () => {
 
           return nextInnerContent;
         });
-      }, 250);
+      }, 200);
     } else {
       const sleep = (delay) =>
         new Promise((resolve) => setTimeout(resolve, delay));
+
       async function test() {
         if (count === 14) {
-          await sleep(3000);
-          setCount(0);
-          setTypingInnerContent("");
-          setTypingChangeFlag(false);
+          await sleep(5000);
+          setCount(12);
         }
+        typingInterval = setInterval(() => {
+          setTypingInnerContent(() => {
+            let nextInnerContent = typingContent.slice(0, count);
+            setCount(count - 1);
+
+            if (count === 0) {
+              setTypingChangeFlag(false);
+              setCount(0);
+              setTypingInnerContent("");
+            }
+
+            return nextInnerContent;
+          });
+        }, 100);
       }
+
       test();
-      // async function test() {
-      //   if (count === 15) {
-      //     await sleep(5000)
-      //     setCount(13)
-      //   }
-      //   typingInterval = setInterval(() => {
-      //     setTypingInnerContent(() => {
-      //       let nextInnerContent = typingContent.slice(0, count)
-      //       setCount(count - 1)
-
-      //       if (count === 0) {
-      //         setTypingChangeFlag(false)
-      //         setCount(0)
-      //         setTypingInnerContent("")
-      //       }
-
-      //       return nextInnerContent
-      //     })
-      //   }, 100)
-      // }
-
-      // test()
     }
 
     return () => {
       clearInterval(typingInterval);
     };
   });
-
-  // 후보2
-  // useEffect(() => {
-  //   let typingInterval;
-  //   if (!typingChangeFlag) {
-  //     typingInterval = setInterval(() => {
-  //       setTypingInnerContent((prevInnerCotent) => {
-  //         let nextInnerContent = prevInnerCotent
-  //           ? prevInnerCotent + typingContent[count]
-  //           : typingContent[0];
-  //         setCount(count + 1);
-
-  //         if (count === typingContent.length - 1) {
-  //           setTypingChangeFlag(true);
-  //         }
-
-  //         return nextInnerContent;
-  //       });
-  //     }, 200);
-  //   } else {
-  //     const sleep = (delay) =>
-  //       new Promise((resolve) => setTimeout(resolve, delay));
-
-  //     async function test() {
-  //       if (count === 14) {
-  //         await sleep(5000);
-  //         setCount(12);
-  //       }
-  //       typingInterval = setInterval(() => {
-  //         setTypingInnerContent(() => {
-  //           let nextInnerContent = typingContent.slice(0, count);
-  //           setCount(count - 1);
-
-  //           if (count === 0) {
-  //             setTypingChangeFlag(false);
-  //             setCount(0);
-  //             setTypingInnerContent("");
-  //           }
-
-  //           return nextInnerContent;
-  //         });
-  //       }, 100);
-  //     }
-
-  //     test();
-  //   }
-
-  //   return () => {
-  //     clearInterval(typingInterval);
-  //   };
-  // });
 
   return (
     <SSection>
