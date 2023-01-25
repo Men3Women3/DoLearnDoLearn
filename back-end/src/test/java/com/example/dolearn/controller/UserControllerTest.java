8a8f269198732d4079df32bd1f51952f3d38d42e
angly97 <angly97@naver.com>
2023-01-25 13:45:48 +0900

    feat : 회원가입 api 이메일 중복 실패 테스트

--- back-end/src/test/java/com/example/dolearn/controller/UserControllerTest.java
+++ back-end/src/test/java/com/example/dolearn/controller/UserControllerTest.java
@@ -2,10 +2,15 @@ package com.example.dolearn.controller;
 
 import com.example.dolearn.config.SecurityConfig;
 import com.example.dolearn.dto.UserDto;
+import com.example.dolearn.exception.CustomException;
+import com.example.dolearn.exception.error.ErrorCode;
 import com.example.dolearn.jwt.JwtTokenProvider;
 import com.example.dolearn.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,13 +20,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest(UserController.class)
 @Import(SecurityConfig.class)
 public class UserControllerTest {
@@ -42,6 +51,7 @@ public class UserControllerTest {
         this.mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity())
+                .alwaysDo(print())
                 .build();
     }
 
@@ -54,15 +64,20 @@ public class UserControllerTest {
 
         @BeforeEach
         void setup(){
-            name = "가입자명";
-            email = "abcd@daum.net";
-            password = "abcdpassord";
+            name = "민싸피";
+            email = "ssafy@naver.com";
+            password = "abcd!1234";
         }
 
         @Test
         @DisplayName("회원가입 성공")
         void success() throws Exception {
+            name = "가입자명";
+            email = "abcd@daum.net";
+            password = "abcdpassord";
+
             UserDto userDto = UserDto.builder().name(name).email(email).password(password).build();
+
             when(userService.signup(any(UserDto.class))).thenReturn(userDto);
 
             mockMvc.perform(post("/user")
@@ -70,6 +85,21 @@ public class UserControllerTest {
                             .with(csrf())
                             .content(toJson(userDto)))
                     .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("회원가입 실패 - 이메일 중복")
+        void failByEmail() throws Exception {
+            UserDto userDto = UserDto.builder().name(name).email(email).password(password).build();
+
+            when(userService.signup(any(UserDto.class))).thenThrow(new CustomException(ErrorCode.EMAIL_DUPLICATION));
+
+            mockMvc.perform(post("/user")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .with(csrf())
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(toJson(userDto)))
+                    .andExpect(status().isConflict());
         }
     }
 
