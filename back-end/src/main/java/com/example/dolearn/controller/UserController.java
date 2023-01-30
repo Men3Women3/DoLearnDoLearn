92d8e585e032f033351dc2bcb795f55d37f4fa74
angly97 <angly97@naver.com>
2023-01-30 17:53:08 +0900

    update : 사용자 정보 수정과 프로필 업로드 api 분리

--- back-end/src/main/java/com/example/dolearn/controller/UserController.java
+++ back-end/src/main/java/com/example/dolearn/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
@@ -97,37 +98,9 @@ public class UserController {
     }
 
     @PutMapping
-    public ResponseEntity<?> update(@RequestPart(value="imgSrc") MultipartFile imgSrc, @Valid @RequestPart(value="userDto") UserDto reqUserDto) {
+    public ResponseEntity<?> update(@Valid @RequestBody UserDto reqUserDto) {
         try{
-            String totalPath = "";      // 저장될 이미지 파일 전체 경로
-
-                                        // 오늘날짜로 폴더 명명
-            String today = new SimpleDateFormat("yyMMdd").format(new Date());
-            String saveFolder = filePath + File.separator + today;
-
-            String saveFileName = "";   // 저장될 이미지 파일 이름
-            String prevFileName = "";   // 현재 저장되어 있는 이미지 파일 이름
-
-            File folder = new File(saveFolder);
-
-            if(imgSrc != null){
-                if(!folder.exists())
-                    folder.mkdirs();
-                prevFileName = userService.getInfo(reqUserDto.getId()).getImgSrc();
-                String originalFileName = imgSrc.getOriginalFilename();
-                saveFileName = System.nanoTime()+originalFileName.substring(originalFileName.lastIndexOf('.'));
-                totalPath = saveFolder + File.separator + saveFileName;
-            }
-            reqUserDto.setImgSrc(totalPath);
-            UserDto userDto = userService.updateInfo(reqUserDto);
-
-            // 기존 파일 삭제 & 새로운 파일 저장
-            if(imgSrc != null){
-               new File(prevFileName).delete();
-                imgSrc.transferTo(new File(folder, saveFileName));
-            }
-
-            return new ResponseEntity<>(new SuccessResponse(userDto), HttpStatus.OK);
+            return new ResponseEntity<>(new SuccessResponse(userService.updateInfo(reqUserDto)), HttpStatus.OK);
         } catch (CustomException e) {
             e.printStackTrace();
             if (e.getErrorCode().getHttpStatus() == HttpStatus.METHOD_NOT_ALLOWED){
@@ -141,6 +114,39 @@ public class UserController {
         }
     }
 
+    @PostMapping("/upload-img/{id}")
+    public ResponseEntity<?> uploadProfileImg(@PathVariable("id") Long id, @RequestPart(value="profileImg") MultipartFile imgSrc) throws IOException {
+        try{
+            // 오늘날짜로 폴더 명명
+            String today = new SimpleDateFormat("yyMMdd").format(new Date());
+            String saveFolder = filePath + File.separator + today;
+
+            File folder = new File(saveFolder);
+            if(!folder.exists())
+                folder.mkdirs();
+
+            // 저장될 파일 명명
+            String originalFileName = imgSrc.getOriginalFilename();
+            String saveFileName = System.nanoTime()+originalFileName.substring(originalFileName.lastIndexOf('.'));
+
+            // 기존 파일 삭제
+            UserDto userDto = userService.getInfo(id);
+            if(!userDto.getImgSrc().equals("")){
+                String prevFileName = userDto.getImgSrc();
+                new File(prevFileName).delete();
+            }
+            
+            // 파일 저장
+            imgSrc.transferTo(new File(folder, saveFileName));
+            userDto.setImgSrc(saveFolder + File.separator + saveFileName);
+
+            return new ResponseEntity<>(new SuccessResponse(userService.updateInfo(userDto)), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
     @PostMapping("/check-email/{email}")
     public ResponseEntity<?> checkEmail(@PathVariable("email") @Email(message = "이메일 형식에 맞지 않습니다.") String email){
         try{
