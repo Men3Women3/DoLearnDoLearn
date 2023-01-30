5012d8a1aedf0da13ab0ade05d810d9462ce67de
young <moxnox63@naver.com>
2023-01-30 12:57:22 +0900

    update: Board API

--- front-end/src/components/BoardList/index.jsx
+++ front-end/src/components/BoardList/index.jsx
@@ -1,28 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { SContainer, SUniDiv } from "./styles";
 import Pagination from "../Pagination";
-import axios from "axios";
 import UniBoard from "../UniBoard";
+import { boardListAPI } from "../../utils/api/boardAPI";
 
 const BoardList = ({ list, setList }) => {
-  // const SERVER_URL = "https://jsonplaceholder.typicode.com/photos";
-  const SERVER_URL = "http://localhost:8080";
-
   // 이 구간은 Pagination을 위해 필요한 부분 ==============
   const limit = 6; // 페이지 당 게시물 수
   const [page, setPage] = useState(1); // 현재 페이지 번호
   const offset = (page - 1) * limit; // 첫 게시물의 위치
   // ======================================================
 
-  // 게시판의 데이터를 받아오는 작업을 하는 부분(boardData)
-  const boardData = async () => {
-    const res = await axios.get(`${SERVER_URL}/board/list`);
-    setList(res.data.response);
-  };
+  // const boardData = async () => {
+  //   const res = await axios.get(`${SERVER_URL}/board/list`);
+  //   setList(res.data.response);
+  // };
 
   useEffect(() => {
-    boardData();
-  }, []);
+    // 게시판의 데이터를 받아오는 작업을 하는 부분(boardList)
+    boardListAPI(setList);
+  }, [list]);
 
   return (
     <>
