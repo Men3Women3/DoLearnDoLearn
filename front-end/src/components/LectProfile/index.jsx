d1b21bdc50edb0b56389587142f5a1f1b1f09750
young <moxnox63@naver.com>
2023-02-02 13:22:13 +0900

    update: 강사 프로필 화면 작업

--- front-end/src/components/LectProfile/index.jsx
+++ front-end/src/components/LectProfile/index.jsx
@@ -4,16 +4,19 @@ import Profile from "../Profile";
 
 const LecProfile = () => {
   // URL의 강사 아이디를 useParama로 받아오기
-  const lid = useParams();
+  const id = useParams().lid;
+
+  // 당사자 정보인 경우에만 프로필 수정 버튼 보이도록 통제
+  const isMe = "other";
 
   // lid로 강사 프로필 정보 가져오기
-
-  console.log(lid);
+  // 1. 위의 id를 바탕으로 강사 정보 데이터 받아오기
+  // 2.
 
   return (
     <div>
       <div>LecProfile</div>
-      {/* <Profile /> */}
+      {/* <Profile  /> */}
     </div>
   );
 };
