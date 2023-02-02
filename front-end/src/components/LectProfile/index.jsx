940b5a4fd49937466537ebc0412c7e2440a96b22
young <moxnox63@naver.com>
2023-02-02 15:27:08 +0900

    update: 강사 프로필 화면 업데이트

--- front-end/src/components/LectProfile/index.jsx
+++ front-end/src/components/LectProfile/index.jsx
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { lecProfileAPI } from "../../utils/api/boardAPI";
 import Profile from "../Profile";
 
 const LecProfile = () => {
   // URL의 강사 아이디를 useParama로 받아오기
   const id = useParams().lid;
-
-  // 당사자 정보인 경우에만 프로필 수정 버튼 보이도록 통제
+  const [data, setData] = useState([]);
   const isMe = "other";
 
-  // lid로 강사 프로필 정보 가져오기
-  // 1. 위의 id를 바탕으로 강사 정보 데이터 받아오기
-  // 2.
+  useEffect(() => {
+    lecProfileAPI(id, setData);
+  }, []);
 
   return (
     <div>
-      <div>LecProfile</div>
-      {/* <Profile  /> */}
+      <Profile handleProfileEditBtn={null} user={data} userState={isMe} />
     </div>
   );
 };
