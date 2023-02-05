229e36a7c9401870579a91405a5df1a73ef6eeb5
YOUNG <moxnox63@gmail.com>
2023-02-06 01:12:59 +0900

    update: 타이머 기능 일부 완성

--- front-end/src/components/BoardList/index.jsx
+++ front-end/src/components/BoardList/index.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 // import createContext from "react";
-import { SContainer, SUniDiv } from "./styles";
-import Pagination from "../Pagination";
-import UniBoard from "../UniBoard";
-import { boardListAPI } from "../../utils/api/boardAPI";
-import { useLocation } from "react-router";
+import { SContainer, SUniDiv } from './styles';
+import Pagination from '../Pagination';
+import UniBoard from '../UniBoard';
+import { boardListAPI } from '../../utils/api/boardAPI';
+import { useLocation } from 'react-router';
+import Timer from '../Timer';
 // export const Flag = createContext();
 
 const BoardList = ({ list, setList }) => {
@@ -17,26 +18,33 @@ const BoardList = ({ list, setList }) => {
   const offset = (page - 1) * limit; // 첫 게시물의 위치
   // ======================================================
 
+  // useEffect(() => {
+  //   if (list.length === 0) return;
+  //   // 게시판의 데이터를 받아오는 작업을 하는 부분(boardList)
+  //   if (location.state?.isWritten === 'true') {
+  //     console.log(location);
+  //     boardListAPI(setList);
+  //     // location.state.isWritten = "false";
+  //   }
+  // }, [flag]);
+
   useEffect(() => {
-    // 게시판의 데이터를 받아오는 작업을 하는 부분(boardList)
-    if (location.state?.isWritten === "true") {
-      console.log(location);
-      boardListAPI(setList);
-      // location.state.isWritten = "false";
-    }
+    if (list.length === 0) return;
+    boardListAPI(setList);
   }, [flag]);
 
   return (
     <>
       {/* <Flag.Provider value={{ flag, setFlag }}> */}
-      <SContainer className="container">
+      <Timer />
+      <SContainer className='container'>
         {/* // offset으로 slicing해서 limit 만큼만 한 화면에 표시 */}
         {list.length > 0
           ? list.slice(offset, offset + limit).map((data) => {
               return (
                 <SUniDiv key={data.id}>
                   <UniBoard
-                    className="uni-board"
+                    className='uni-board'
                     data={data}
                     flag={flag}
                     setFlag={setFlag}
@@ -45,7 +53,7 @@ const BoardList = ({ list, setList }) => {
                 </SUniDiv>
               );
             })
-          : ""}
+          : ''}
       </SContainer>
       {list.length < 7 ? null : (
         <Pagination
