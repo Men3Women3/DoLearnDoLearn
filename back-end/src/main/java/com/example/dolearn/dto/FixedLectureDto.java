998e6a17224f4664057d599fd18067de7f6fa774
angly97 <angly97@naver.com>
2023-02-05 10:40:16 +0900

    update : get fixed lecture api 수정 (출력 컬럼 추가)

--- back-end/src/main/java/com/example/dolearn/dto/FixedLectureDto.java
+++ back-end/src/main/java/com/example/dolearn/dto/FixedLectureDto.java
@@ -14,9 +14,23 @@ public class FixedLectureDto {
 
     Long id;
 
+    private Long bid;
+
+    private Long uid;
+
     private String title;
 
+    private String content;
+
     private String summary;
+
+    private int maxCnt;
+
+    private int students;
+
+    private int instructors;
+
+    private int isFixed;
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date startTime;
@@ -24,11 +38,28 @@ public class FixedLectureDto {
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 
-    public FixedLectureDto(Long id, String title, String summary, Date startTime, Date endTime) {
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date deadline;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date createdTime;
+
+    public FixedLectureDto(Long id, Long bid, Long uid, String title, String content, String summary,
+                           int maxCnt, long instructors, long students, int isFixed,
+                           Date startTime, Date endTime, Date deadline, Date createdTime) {
         this.id = id;
+        this.bid = bid;
+        this.uid = uid;
         this.title = title;
+        this.content = content;
         this.summary = summary;
+        this.maxCnt = maxCnt;
+        this.instructors = (int)instructors;
+        this.students = (int)students;
+        this.isFixed = isFixed;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.deadline = deadline;
+        this.createdTime = createdTime;
     }
 }
