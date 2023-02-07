7bc40d035a71524a161642b894400c5bcf8659e2
김정원 <wjddnjs0528@naver.com>
2023-02-07 17:53:50 +0900

    update : 마이페이지 일정 모달 수정 중

--- front-end/src/components/LectureFixedModal/index.jsx
+++ front-end/src/components/LectureFixedModal/index.jsx
@@ -6,10 +6,9 @@ import {
   faPersonChalkboard,
   faUsers,
 } from "@fortawesome/free-solid-svg-icons";
-import { SContent } from "../MessageDetailModal/styles";
 import { SButtonBox, SButton } from "../LectureModalButton/styles";
 import defaultProfile from "../../assets/images/defaultProfile.png";
-import { SCustomFontAwesomeIcon, SSpan, SInfoItem } from "./styles";
+import { SCustomFontAwesomeIcon, SSpan, SInfoItem, SContent } from "./styles";
 import { imageURL } from "../../utils/api/baseURL";
 
 const style = {
@@ -42,65 +41,63 @@ const LectureFixedModal = ({
           </Typography>
           <hr />
           <SContent>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <SInfoItem>
-                <SCustomFontAwesomeIcon icon={faPersonChalkboard} />
-                <SSpan>
-                  <b>강사 정보</b>
-                </SSpan>
-              </SInfoItem>
-              <div style={{ display: "flex" }}>
-                <div>
-                  <img
-                    src={
-                      instructorInfo.user.imgUrl
-                        ? `${imageURL}${instructorInfo.user.imgUrl}`
-                        : defaultProfile
-                    }
-                    style={{ width: "50px" }}
-                  />
-                </div>
-                <div>
-                  <div>{instructorInfo.user.name}</div>
-                  <div>{instructorInfo.user.email}</div>
-                </div>
-              </div>
-            </div>
-
-            <br />
-            {/* <div className="down__container"> */}
-            <div>
-              <SInfoItem>
-                <SCustomFontAwesomeIcon icon={faUsers} />
-                <SSpan>
-                  <b>수강 인원</b> | 3명
-                </SSpan>
-              </SInfoItem>
-              <SInfoItem>
-                <SCustomFontAwesomeIcon icon={faClock} />
-                <SSpan>
-                  <b>강의 시간</b> | 23.02.15 01:00 (1시간)
-                </SSpan>
-              </SInfoItem>
+            <SInfoItem>
+              <SCustomFontAwesomeIcon icon={faPersonChalkboard} />
+              <SSpan>
+                <b>강사 정보</b>
+              </SSpan>
+            </SInfoItem>
+            <div className="instructor__section">
               <div>
-                <SInfoItem>
-                  <SCustomFontAwesomeIcon
-                    icon={faFileLines}
-                    style={{ height: "calc(1vw + 4px)" }}
-                  />
-                  <SSpan>
-                    <b>강의 상세 내용</b>
-                  </SSpan>
-                </SInfoItem>
-                <div className="custom-content">
-                  <div>
-                    dkajflkdajflkajl;fjkafld;sanvmxz.nvmncakljfkldafkladln어러러어ㅇㅇㅇㅇㅇㅇ
-                  </div>
+                <img
+                  className="profile-img"
+                  src={
+                    instructorInfo.user.imgUrl
+                      ? `${imageURL}${instructorInfo.user.imgUrl}`
+                      : defaultProfile
+                  }
+                  alt=""
+                />
+              </div>
+              <div>
+                <div className="instructor-name">
+                  {instructorInfo.user.name}
+                </div>
+                <div className="instructor-email">
+                  {instructorInfo.user.email}
                 </div>
               </div>
-              <div></div>
             </div>
           </SContent>
+
+          <div>
+            <SInfoItem>
+              <SCustomFontAwesomeIcon icon={faUsers} />
+              <SSpan>
+                <b>수강 인원</b> | {studentsInfo.length}명
+              </SSpan>
+            </SInfoItem>
+            <SInfoItem>
+              <SCustomFontAwesomeIcon icon={faClock} />
+              <SSpan>
+                <b>강의 시간</b> | 23.02.15 01:00 (1시간)
+              </SSpan>
+            </SInfoItem>
+            <div>
+              <SInfoItem>
+                <SCustomFontAwesomeIcon icon={faFileLines} />
+                <SSpan>
+                  <b>강의 상세 내용</b>
+                </SSpan>
+              </SInfoItem>
+              <div className="custom-content">
+                <div>
+                  dkajflkdajflkajl;fjkafld;sanvmxz.nvmncakljfkldafkladln어러러어ㅇㅇㅇㅇㅇㅇ
+                </div>
+              </div>
+            </div>
+            <div></div>
+          </div>
           <SButtonBox>
             <SButton>신청취소</SButton>
             <SButton>Live 입장</SButton>
--- front-end/src/components/LectureFixedModal/styles.jsx
+++ front-end/src/components/LectureFixedModal/styles.jsx
@@ -1,18 +1,47 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
+export const SContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: ${(props) => props.theme.fontFamily.Medium};
+  .instructor__section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    margin-bottom: 1vw;
+    background-color: ${(props) => props.theme.lightYellow};
+    padding: 1vw;
+  }
+  .profile-img {
+    width: 4vw;
+    height: 4vw;
+    border-radius: 50%;
+    border: 2px solid black;
+    background-color: white;
+  }
+  .instructor-name {
+    font-size: ${(props) => props.theme.fontSize.h3};
+  }
+  .instructor-email {
+    font-size: ${(props) => props.theme.fontSize.p};
+    color: gray;
+  }
+`;
+
 export const SCustomFontAwesomeIcon = styled(FontAwesomeIcon)`
   width: 2vw;
 `;
 
 export const SSpan = styled.span`
   font-family: ${(props) => props.theme.fontFamily.Medium};
-  font-size: ${(props) => props.theme.fontSize.h3};
+  font-size: ${(props) => props.theme.fontSize.p};
 `;
 
 export const SInfoItem = styled.div`
   display: flex;
   column-gap: 5px;
   align-items: center;
-  margin-bottom: calc(0.8vw + 1px);
+  margin-bottom: 1vw;
 `;
