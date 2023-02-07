506a2bb4259a460d22f3886d7b73a7916b3b9039
young <moxnox63@naver.com>
2023-02-07 15:59:39 +0900

    update: 강사 프로필 병아리 gif

--- front-end/src/pages/LecturerProfile/styles.jsx
+++ front-end/src/pages/LecturerProfile/styles.jsx
@@ -1,16 +1,18 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const SContainer = styled.div`
   height: 100%;
   padding-top: 1.5rem;
-  background-image: linear-gradient(147deg, #ff5825 0%, #ffe53b 74%);
+  /* background-image: linear-gradient(147deg, #ff5825 0%, #ffe53b 74%); */
+  background: linear-gradient(
+    90deg,
+    rgba(255, 232, 154, 1) 25%,
+    rgba(255, 153, 98, 1) 100%
+  );
   .headline {
     border-bottom: 6px solid #ffc29f;
     font-family: "GmarketSansBold";
     color: black;
     margin-bottom: 30px;
-    /* background-image: linear-gradient(147deg, #ff5825 0%, #ffe53b 74%);
-    color: transparent;
-    -webkit-background-clip: text; */
   }
-`
+`;
