5b3c0a65d3914486556b74233b8c8ae009c68e2b
unknown <pjw2369@naver.com>
2023-02-07 13:05:55 +0900

    update : 충돌 해결

--- front-end/src/components/LectureModalButton/index.jsx
+++ front-end/src/components/LectureModalButton/index.jsx
@@ -17,11 +17,12 @@ import {
 } from "../../utils/api/boardAPI";
 // import { sendMessageAPI } from "../../utils/api/messageAPI";
 
-const LectureModalButton = ({ data, setOpen, flag, setFlag, Luid }) => {
+const LectureModalButton = ({ data, setOpen, Luid }) => {
+  const { flag, setFlag } = useContext(BoardDataContext);
+  const [check, setCheck] = useState(false);
   const { isLogined, userInfo } = useContext(LoginStateContext);
   const { unreadMessageCnt, setStateMessageUpdate } =
     useContext(UnreadMessageContext);
-  // const { flag, setFlag } = useContext(Flag);
 
   // api 요청 내용 ===================================
   // 수강 신청
@@ -47,16 +48,17 @@ const LectureModalButton = ({ data, setOpen, flag, setFlag, Luid }) => {
 
   // 신청 취소
   const cancelClass = async () => {
-    await cancelEnrollAPI(userInfo.id, data.id);
+    await cancelEnrollAPI(userInfo.id, data.id, setCheck);
     setFlag(!flag);
     setOpen(false);
   };
 
   // 모집 완료
   const fixClass = async () => {
-    await fixClassAPI(data.id, Luid, setStateMessageUpdate);
+    await fixClassAPI(data.id, Luid);
     setFlag(!flag);
     setOpen(false);
+    setStateMessageUpdate(true);
   };
 
   // 강사 목록 호출
@@ -67,29 +69,55 @@ const LectureModalButton = ({ data, setOpen, flag, setFlag, Luid }) => {
   useEffect(() => {
     lecturerNameAPI(data.id, setNameList);
     stuListAPI(data.id, setStuList);
-    lecListAPI(data.id, setLecList);
+    lecListAPI(data.id, setLecList, setCheck);
   }, []);
 
   // =================================================
 
-  if (data.uid === userInfo.id) {
-    // 방장이고, 모집완료 이전
-    if (data.isFixed === 0) {
-      return (
-        <>
-          {/* 신청 강사 목록이 비어있지 않은 경우에는 목록을 보여주고 그 외에는 공백 */}
+  if (!isLogined) {
+    return "";
+  } else {
+    if (data.uid === userInfo.id) {
+      // 방장이고, 모집완료 이전
+      if (data.isFixed === 0) {
+        return (
+          <>
+            {/* 신청 강사 목록이 비어있지 않은 경우에는 목록을 보여주고 그 외에는 공백 */}
+            <SButtonBox>
+              <SButton onClick={deleteClass}>강의삭제</SButton>
+              {Luid === "none" ? (
+                ""
+              ) : (
+                <SButton onClick={fixClass}>모집완료</SButton>
+              )}
+            </SButtonBox>
+          </>
+        );
+        // 방장이고, 모집완료 이후
+      } else {
+        return (
           <SButtonBox>
-            <SButton onClick={deleteClass}>강의삭제</SButton>
-            {Luid === "none" ? (
-              ""
-            ) : (
-              <SButton onClick={fixClass}>모집완료</SButton>
-            )}
+            <SButton onClick={cancelClass}>신청취소</SButton>
+            <SButton>Live 입장</SButton>
           </SButtonBox>
-        </>
-      );
-      // 방장이고, 모집완료 이후
-    } else {
+        );
+      }
+    } else if (stuList.includes(userInfo.id) || lecList.includes(userInfo.id)) {
+      if (data.isFixed === 0) {
+        return (
+          <SButtonBox>
+            <SButton onClick={cancelClass}>신청취소</SButton>
+          </SButtonBox>
+        );
+      } else {
+        return (
+          <SButtonBox>
+            <SButton onClick={cancelClass}>신청취소</SButton>
+            <SButton>Live 입장</SButton>
+          </SButtonBox>
+        );
+      }
+    } else if (check) {
       return (
         <SButtonBox>
           <SButton onClick={enrollLecturer}>강사 신청</SButton>
@@ -101,30 +129,6 @@ const LectureModalButton = ({ data, setOpen, flag, setFlag, Luid }) => {
         </SButtonBox>
       );
     }
-  } else if (stuList.includes(userInfo.id) || lecList.includes(userInfo.id)) {
-    if (data.isFixed === 0) {
-      return (
-        <SButtonBox>
-          <SButton onClick={cancelClass}>신청취소</SButton>
-        </SButtonBox>
-      );
-    } else {
-      return (
-        <SButtonBox>
-          <SButton onClick={cancelClass}>신청취소</SButton>
-          <SButton>Live 입장</SButton>
-        </SButtonBox>
-      );
-    }
-  } else if (isLogined) {
-    return (
-      <SButtonBox>
-        <SButton onClick={enrollLecturer}>강사 신청</SButton>
-        <SButton onClick={enrollClass}>수강생 신청</SButton>
-      </SButtonBox>
-    );
-  } else {
-    return "";
   }
 };
 
