109cf2b33f97fc326e159d2bee02b7a8071a26e6
angly97 <angly97@naver.com>
2023-02-07 11:36:56 +0900

    feat : 포인트순으로 유저 정렬 조회 api 구현

--- back-end/src/main/java/com/example/dolearn/repository/UserRepository.java
+++ back-end/src/main/java/com/example/dolearn/repository/UserRepository.java
@@ -11,5 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findOneByEmail(String email);
+
     Optional<User> findOneById(Long id);
 }
