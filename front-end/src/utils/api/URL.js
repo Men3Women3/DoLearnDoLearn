ea7eae26c2a0f2658508fc31db402267260fa162
young <moxnox63@naver.com>
2023-02-09 16:49:01 +0900

    update: url 수정

--- front-end/src/utils/api/URL.js
+++ front-end/src/utils/api/URL.js
@@ -1,4 +1,3 @@
-export const BASE_URL = "http://localhost:8080/api";
-export const IMAGE_URL = "http://localhost:8080";
-export const WEBRTC_URL = "wss://i8a802.p.ssafy.io//groupcall";
-export const SOCKET_URL = "http://localhost:8080/ws";
+export const BASE_URL = "https://i8a802.p.ssafy.io/api";
+export const WEBRTC_URL = "wss://i8a802.p.ssafy.io/groupcall";
+export const SOCKET_URL = "https://i8a802.p.ssafy.io/ws";
