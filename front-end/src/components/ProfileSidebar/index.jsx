b50be502e478cc365f2469b0da76408f50f14c9c
JW <wjddnjs0528@gmail.com>
2023-02-12 23:44:54 +0900

    update: 마이페이지 시계 컴포넌트 추가

--- front-end/src/components/ProfileSidebar/index.jsx
+++ front-end/src/components/ProfileSidebar/index.jsx
@@ -14,6 +14,7 @@ import WarningModal from "../WarningModal"
 import { deleteUserAPI } from "../../utils/api/userAPI"
 import { useContext } from "react"
 import { LoginStateHandlerContext } from "../../App"
+import ProfileClock from "../ProfileClock"
 
 const ProfileSidebar = (props) => {
   const { handleSnackbarInfo } = useContext(LoginStateHandlerContext)
@@ -27,88 +28,101 @@ const ProfileSidebar = (props) => {
   }
 
   return (
-    <SSidebarContainer>
-      {/* 프로필 탭 */}
-      <Link to={"/mypage"} state="main" style={{ textDecoration: "none" }}>
-        <SButtonContainer>
-          <div
-            className={
-              props.isProfileTabActive
-                ? "active tab__section profile-page"
-                : "tab__section tab-content profile-page"
-            }
-            onClick={props.handleTabValue}
+    <>
+      <SSidebarContainer>
+        <div className="sidebar__container">
+          {/* 프로필 탭 */}
+          <Link to={"/mypage"} state="main" style={{ textDecoration: "none" }}>
+            <SButtonContainer>
+              <div
+                className={
+                  props.isProfileTabActive
+                    ? "active tab__section profile-page"
+                    : "tab__section tab-content profile-page"
+                }
+                onClick={props.handleTabValue}
+              >
+                <FontAwesomeIcon icon={faUser} />
+                &emsp;프로필
+              </div>
+            </SButtonContainer>
+          </Link>
+          {/* 일정 탭 */}
+          <Link
+            to={"/mypage"}
+            state="schedule"
+            style={{ textDecoration: "none" }}
           >
-            <FontAwesomeIcon icon={faUser} />
-            &emsp;프로필
-          </div>
-        </SButtonContainer>
-      </Link>
-      {/* 일정 탭 */}
-      <Link to={"/mypage"} state="schedule" style={{ textDecoration: "none" }}>
-        <SButtonContainer>
-          <div
-            className={
-              props.isScheduleTabActive
-                ? "active tab__section schedule-page"
-                : "tab__section tab-content schedule-page"
-            }
-            onClick={props.handleTabValue}
-          >
-            <FontAwesomeIcon icon={faCalendarCheck} />
-            &emsp;일정
-          </div>
-        </SButtonContainer>
-      </Link>
+            <SButtonContainer>
+              <div
+                className={
+                  props.isScheduleTabActive
+                    ? "active tab__section schedule-page"
+                    : "tab__section tab-content schedule-page"
+                }
+                onClick={props.handleTabValue}
+              >
+                <FontAwesomeIcon icon={faCalendarCheck} />
+                &emsp;일정
+              </div>
+            </SButtonContainer>
+          </Link>
 
-      {/* 미확정 강의 탭 */}
-      <Link
-        to={"/mypage"}
-        state="unschedule"
-        style={{ textDecoration: "none" }}
-      >
-        <SButtonContainer>
-          <div
-            className={
-              props.isUnScheduleTabActive
-                ? "active tab__section undecided-lecture-page"
-                : "tab__section tab-content undecided-lecture-page"
-            }
-            onClick={props.handleTabValue}
+          {/* 미확정 강의 탭 */}
+          <Link
+            to={"/mypage"}
+            state="unschedule"
+            style={{ textDecoration: "none" }}
           >
-            <FontAwesomeIcon icon={faCalendarXmark} />
-            &emsp;미확정 강의
-          </div>
-        </SButtonContainer>
-      </Link>
-      {/* 메시지함 탭 */}
-      <Link to={"/mypage"} state="message" style={{ textDecoration: "none" }}>
-        <SButtonContainer>
-          <div
-            className={
-              props.isMessageTabActive
-                ? "active tab__section message-page"
-                : "tab__section tab-content message-page"
-            }
-            onClick={props.handleTabValue}
+            <SButtonContainer>
+              <div
+                className={
+                  props.isUnScheduleTabActive
+                    ? "active tab__section undecided-lecture-page"
+                    : "tab__section tab-content undecided-lecture-page"
+                }
+                onClick={props.handleTabValue}
+              >
+                <FontAwesomeIcon icon={faCalendarXmark} />
+                &emsp;미확정 강의
+              </div>
+            </SButtonContainer>
+          </Link>
+          {/* 메시지함 탭 */}
+          <Link
+            to={"/mypage"}
+            state="message"
+            style={{ textDecoration: "none" }}
           >
-            <FontAwesomeIcon icon={faEnvelope} />
-            &emsp;메시지함
-          </div>
-        </SButtonContainer>
-      </Link>
-      {/* 회원탈퇴 버튼 */}
-      <SUserDeleteButtonContainer>
-        {/* ProfileSidebar에서 사용시 */}
-        <WarningModal
-          title="회원 탈퇴 확인"
-          warningContent="회원 탈퇴 후에는 아이디와 데이터를 복구할 수 없습니다."
-          content="탈퇴를 원하시면 확인을 눌러주세요."
-          profileSidebar
-          handler={handleDeleteUser}
-        />
-      </SUserDeleteButtonContainer>
-    </SSidebarContainer>
+            <SButtonContainer>
+              <div
+                className={
+                  props.isMessageTabActive
+                    ? "active tab__section message-page"
+                    : "tab__section tab-content message-page"
+                }
+                onClick={props.handleTabValue}
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+                &emsp;메시지함
+              </div>
+            </SButtonContainer>
+          </Link>
+          {/* 회원탈퇴 버튼 */}
+          <SUserDeleteButtonContainer>
+            {/* ProfileSidebar에서 사용시 */}
+            <WarningModal
+              title="회원 탈퇴 확인"
+              warningContent="회원 탈퇴 후에는 아이디와 데이터를 복구할 수 없습니다."
+              content="탈퇴를 원하시면 확인을 눌러주세요."
+              profileSidebar
+              handler={handleDeleteUser}
+            />
+          </SUserDeleteButtonContainer>
+        </div>
+        <ProfileClock />
+      </SSidebarContainer>
+    </>
   )
 }
 
