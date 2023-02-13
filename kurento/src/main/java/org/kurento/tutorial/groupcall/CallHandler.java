8128f629f14796c4a3b1a02278e533f694330439
찬휘 <cksgnlcjswoo@naver.com>
2023-02-13 15:24:22 +0900

    update kurento

--- kurento/src/main/java/org/kurento/tutorial/groupcall/CallHandler.java
+++ kurento/src/main/java/org/kurento/tutorial/groupcall/CallHandler.java
@@ -51,9 +51,13 @@ public class CallHandler extends TextWebSocketHandler {
 
   @Override
   public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    System.out.println("handleTextMessage session : " + session);
+
     final JsonObject jsonMessage = gson.fromJson(message.getPayload(), JsonObject.class);
+    System.out.println("handleTextMessage jsonMessage : " + jsonMessage);
 
     final UserSession user = registry.getBySession(session);
+    System.out.println("handleTextMessage user : " + user);
 
     if (user != null) {
       log.debug("Incoming message from user '{}': {}", user.getName(), jsonMessage);
@@ -63,50 +67,36 @@ public class CallHandler extends TextWebSocketHandler {
 
     switch (jsonMessage.get("id").getAsString()) {
       case "joinRoom":
+        System.out.println("joinRoom");
         joinRoom(jsonMessage, session);
-        UserSession userSession = registry.getBySession(session);
-        Room room = roomManager.getRoom(userSession.getRoomName());
-        for (UserSession participant : room.getParticipants()) {
-          participant.sendMessage(jsonMessage);
-        }
         break;
       case "receiveVideoFrom":
+        System.out.println("receiveVideoFrom");
         final String senderName = jsonMessage.get("sender").getAsString();
         final UserSession sender = registry.getByName(senderName);
         final String sdpOffer = jsonMessage.get("sdpOffer").getAsString();
         user.receiveVideoFrom(sender, sdpOffer);
         break;
       case "leaveRoom":
-        userSession = registry.getBySession(session);
-        room = roomManager.getRoom(userSession.getRoomName());
-        for (UserSession participant : room.getParticipants()) {
-          participant.sendMessage(jsonMessage);
-        }
+        System.out.println("leaveRoom");
         leaveRoom(user);
-        break;
-      case "leaveRoomToShareScreen":
-        leaveRoom(user);
-        break;
-      case "helpUser":
-        userSession = registry.getBySession(session);
-        room = roomManager.getRoom(userSession.getRoomName());
-        for (UserSession participant : room.getParticipants()) {
-          participant.sendMessage(jsonMessage);
-        }
-        break;
-      case "shareScreen":
-        shareScreen(jsonMessage, session);
         break;
       case "onIceCandidate":
+        System.out.println("onIceCandidate");
         JsonObject candidate = jsonMessage.get("candidate").getAsJsonObject();
 
         if (user != null) {
           IceCandidate cand = new IceCandidate(candidate.get("candidate").getAsString(),
-              candidate.get("sdpMid").getAsString(), candidate.get("sdpMLineIndex").getAsInt());
+                  candidate.get("sdpMid").getAsString(), candidate.get("sdpMLineIndex").getAsInt());
           user.addCandidate(cand, jsonMessage.get("name").getAsString());
         }
         break;
+      case "shareScreen":
+        System.out.println("shareScreen");
+        shareScreen(jsonMessage, session);
+        break;
       default:
+        System.out.println("default");
         break;
     }
   }
@@ -135,13 +125,13 @@ public class CallHandler extends TextWebSocketHandler {
     }
   }
 
-	private void shareScreen (JsonObject params, WebSocketSession session) throws IOException {
-		final String roomName = params.get("room").getAsString();
-		final String name = params.get("name").getAsString();
-		log.info("PARTICIPANT {}: trying to join room {}", name, roomName);
+  private void shareScreen (JsonObject params, WebSocketSession session) throws IOException {
+    final String roomName = params.get("room").getAsString();
+    final String name = params.get("name").getAsString();
+    log.info("PARTICIPANT {}: trying to join room {}", name, roomName);
 
-		Room room = roomManager.getRoom(roomName);
-		final UserSession user = room.join("screen"+name, session);
-		registry.register(user);
-	}
+    Room room = roomManager.getRoom(roomName);
+    final UserSession user = room.join("screen"+name, session);
+    registry.register(user);
+  }
 }
